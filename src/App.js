2f363581c8a599f8a144dc05cefedba5d7c278ad
Gaurang <gaurang@pop-os.localdomain>
2021-09-21 12:00:44 +0530

    Added topbar and main content structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './App.scss';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+    <div className="topbar">
+
+        <div>
+            ABC
+        </div>
+
     </div>
+
+    <div className="main-content">
+
+        <div>
+            Content
+        </div>
+
+    </div>
+
+
+</div>
+
   );
 }
 
